0f319a26831756184a9e74272201fff476a01919
quanganh-snake <tbquanganh@gmail.com>
2024-10-24 17:21:06 +0700

    config(ui): using glustack-ui v2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
+import "'./global.css'";
+import { GluestackUIProvider } from "@/'components/ui'/gluestack-ui-provider";
 import React from 'react';
 
 const App = () => {
 	return (
-		<View>
-			<Text>App</Text>
-		</View>
-	);
+        <View>
+            <Text>App</Text>
+        </View>
+    );
 };
 
 export default App;
